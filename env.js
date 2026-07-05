63444e520c90dbd5f33065deb1fb086aed173d46
Presi <163936587+Presi81@users.noreply.github.com>
2026-07-05 18:57:10 +0200

    Update env.js

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 // CONFIGURACIÓN DE ENTORNO - ¡MÁXIMA SEGURIDAD!
 window.env = {
-  SUPABASE_URL: "Pon tu url",
-  SUPABASE_ANON_KEY: "pon tu anon key",
-  GEMINI_API_KEY: "pon tu api de gemi"
+  SUPABASE_URL: "",
+  SUPABASE_ANON_KEY: "",
+  GEMINI_API_KEY: ""
 };
